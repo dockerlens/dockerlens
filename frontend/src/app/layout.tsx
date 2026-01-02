7cb9b3333fa44aa42b1b43b0b2bd55adf8e241e9
slxca <straeter.luca@gmail.com>
2026-01-02 04:33:22 +0100

    initialize project structure with essential configuration files and basic layout

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,12 @@
+import {ReactNode} from "react";
+import "./globals.css"
+
+export default function RootLayout({ children } : { children: ReactNode }) {
+    return (
+        <html lang="en">
+        <body className={'antialiased'}>
+        {children}
+        </body>
+        </html>
+    );
+}
